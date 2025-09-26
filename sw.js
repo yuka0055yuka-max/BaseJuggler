edf54e4bf8c1b47234a21ed601c98e17a9843863
yuka0055yuka-max <yuka0055yuka@gmail.com>
2025-09-27 00:04:17 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,51 +1,24 @@
-// キャッシュの名前とバージョンを定義
-const CACHE_NAME = 'base-converter-cache-v1';
-
-// オフライン時に利用できるようにキャッシュするファイルのリスト
+const CACHE_NAME = 'mirror_fake_cache_v1';
 const urlsToCache = [
-    '/BaseJuggler/',
-    '/BaseJuggler/index.html',
-    '/BaseJuggler/style.css',
-    '/BaseJuggler/script.js',
-    '/BaseJuggler/manifest.json',
-    '/BaseJuggler/icon-192.png',
-    '/BaseJuggler/icon-512.png'
+  '/BaseJuggler/',
+  '/BaseJuggler/index.html',
+  '/BaseJuggler/script.js',
+  '/BaseJuggler/exif-stripper.js',
+  '/BaseJuggler/exif-js.min.js',
+  '/BaseJuggler/manifest.json',
+  '/BaseJuggler/icon-192.png',
+  '/BaseJuggler/icon-512.png'
 ];
 
-// Service Workerのインストールイベント
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-// Service Workerのフェッチイベント
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
-});
-
-// Service Workerの有効化イベント
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
 
